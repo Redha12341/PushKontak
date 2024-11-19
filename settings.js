f743cf1a47b754fd0166ebdb845da41e4de4cde5
Redha12341 <redarodha@gmail.com>
2024-11-19 15:26:00 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const fs = require('fs')
 
 global.fotomenfess = fs.readFileSync("./menfess.jpg")
-global.owner = ["6283851705636"]
+global.owner = ["6283863423968"]
 
 global.help = {
 Menu(pushname){
-  return `Halo ${pushname}
+  return `Halo ${ℜ𝔢𝔡 𝔖𝔱𝔬𝔯𝔢}
 
 Saya adalah bot WhatsApp confess 👋 ketik .confess untuk memulai chat.
 `
